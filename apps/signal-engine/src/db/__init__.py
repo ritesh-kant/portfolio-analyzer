130b769a2d367d33821d69d02843668bd0d9089b
ritesh kant <ritesh.kant47@gmail.com>
2026-05-27 21:24:15 +0530

    refactor(signal-engine): remove unused repository files and constants

--- apps/signal-engine/src/db/__init__.py
+++ apps/signal-engine/src/db/__init__.py
@@ -1,6 +1,5 @@
 """Motor async MongoDB client package."""
 
 from .client import get_client, get_db, close_client
-from .constants import COLLECTION_NAMES
 
-__all__ = ["get_client", "get_db", "close_client", "COLLECTION_NAMES"]
+__all__ = ["get_client", "get_db", "close_client"]
